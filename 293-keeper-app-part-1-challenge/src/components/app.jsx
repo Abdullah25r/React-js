13703604863fbf2a56283f343c99bd71741f0496
Abdullah Rasheed <bhatti253r@gmail.com>
2024-07-14 12:04:47 +0500

    complete keeper app first phase

---
+++ 293-keeper-app-part-1-challenge/src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Note from "./note";
+import Header from "./header";
+import Footer from "./footer";
+function App() {
+    return(
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    );
+}
+export default App;
---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>hello world</h1>
+    <p>This is a paragraph element</p>
+  </div>,
+  document.getElementById("root")
+);
